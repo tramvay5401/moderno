bc450a5ce6e9a0bc98392d7916e562fede769b41
tramvay5401 <tramvay5401@gmail.com>
2019-11-25 08:26:12 +0200

    start project

---
+++ gulpfile.js
@@ -0,0 +1,50 @@
+let gulp = require("gulp"),
+  sass = require("gulp-sass"),
+  browserSync = require("browser-sync"),
+  uglify = require("gulp-uglify"),
+  concat = require("gulp-concat"),
+  rename = require("gulp-rename");
+
+gulp.task("js", function() {
+       return gulp.src([
+      "node_modules/slick-carousel/slick/slick.js",
+      "node_modules/magnific-popup/dist/jquery.magnific-popup.js"
+    ])
+    .pipe(concat("libs.min.js"))
+    .pipe(uglify())
+    .pipe(gulp.dest("app/js"))
+    .pipe(browserSync.reload({ stream: true }));
+});
+
+gulp.task("scss", function() {
+  return gulp
+    .src("app/scss/**/*.scss")
+    .pipe(rename({suffix:".min"}))
+    .pipe(sass({ outputStyle: "compressed" }))
+    .pipe(gulp.dest("app/css"))
+    .pipe(browserSync.reload({ stream: true }));
+});
+
+gulp.task("html", function() {
+  return gulp.src("app/*.html").pipe(browserSync.reload({ stream: true }));
+});
+
+gulp.task("script", function() {
+  return gulp.src("app/js/*.js").pipe(browserSync.reload({ stream: true }));
+});
+
+gulp.task("browser-sync", function() {
+  browserSync.init({
+    server: {
+      baseDir: "app/"
+    }
+  });
+});
+
+gulp.task("watch", function() {
+  gulp.watch("app/scss/**/*.scss", gulp.parallel("scss"));
+  gulp.watch("app/*.html", gulp.parallel("html"));
+  gulp.watch('app/js/*.js', gulp.parallel('script'))
+});
+
+gulp.task("default", gulp.parallel("scss","js","browser-sync", "watch"));
