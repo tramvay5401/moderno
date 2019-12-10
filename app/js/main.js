eeff715d5982dfa83345da478c096f73dc9ae2e7
tramvay5401 <tramvay5401@gmail.com>
2019-12-10 09:04:02 +0200

    added new-section and adaptive header and footer

--- app/js/main.js
+++ app/js/main.js
@@ -33,20 +33,13 @@ $(function() {
     $('.icon-th-list').removeClass('active');
   });
   
-  // $(".icon-th-list").on("click", function() {
-  //   $('.icon-th-list').addClass('active')
-  // });
-
-  // $('.icon-th-large').on('click',function(){
-  //   $('.icon-th-list').removeClass('active')
-  // });
-  // $(".icon-th-large").on("click", function() {
-  //   $('.icon-th-large').addClass('active')
-  // });
-
-  // $('.icon-th-list').on('click',function(){
-  //   $('.icon-th-large').removeClass('active')
-  // });
+  $(".menu__btn").on('click', function(){
+    $('.menu__list').slideToggle();
+  });
+  
+  $(".header__btn-menu").on('click', function(){
+    $('.header__box').toggleClass('active');
+  });
 
 
   var mixer = mixitup(".products__inner-box");
